13f328bab5fe5b96c1816c5006b2336719ef142a
admin <ignatenkobogdan88@gmail.com>
2024-06-27 16:42:48 +0300

    2

--- _path.py
+++ _path.py
@@ -1,3 +1,3 @@
 _path = "F:\\Chrom_расширения\\coordinates.crx"
 profile_path = '--user-data-dir=C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1'
-proxy = "159.224.232.194:8888"
+proxy = "91.220.81.99:63708"
--- main.py
+++ main.py
@@ -4,27 +4,20 @@ import time
 
 from _path import _path, profile_path, proxy
 
-
 try:
     options_chrome = webdriver.ChromeOptions()
+    # browser.refresh()     # перезагрузка странички браузера
     # options_chrome.add_extension(f'{_path}')
     # options_chrome.add_argument(f"{profile_path}")
-    # options_chrome.add_argument('--proxy-server=%s' % proxy)
+    options_chrome.add_argument('--proxy-server=%s' % proxy)
     # options_chrome.add_argument('--headless')     # работает без графического интерфейса
-    
     options_chrome.add_argument('--start-maximized')    # устанавливает окно браузера в максимальном режиме
 
     browser = webdriver.Chrome(options=options_chrome)
-    # browser.get('https://parsinger.ru/methods/1/index.html')
-    browser.get('https://2ip.ru/')
-    time.sleep(100000)
-    per = browser.find_element(By.ID, 'result').text
-    while per is None:
-        browser.refresh()
-        time.sleep(2)
-        per = browser.find_element(By.ID, 'result').text
-
-    print(per)
+    browser.get('https://parsinger.ru/selenium/5.5/1/1.html')
+    time.sleep(1)
+    for field in browser.find_elements(By.CLASS_NAME, 'text-field'):
+        field.clear()
 
 
 finally:
